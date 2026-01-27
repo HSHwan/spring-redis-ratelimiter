450031dd233a9cbafb5a488651206f7dcb78715d
HSHwan <tnghks2866@gmail.com>
2026-01-27 10:51:18 +0900

    test: RateLimiterService 단위 테스트 생성자 주입 적용

--- src/test/java/com/keeper/ratelimiter/service/RateLimiterServiceTest.java
+++ src/test/java/com/keeper/ratelimiter/service/RateLimiterServiceTest.java
@@ -1,12 +1,14 @@
 package com.keeper.ratelimiter.service;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.mockito.ArgumentMatchers.any;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -17,7 +19,6 @@ import org.springframework.data.redis.core.script.RedisScript;
 @ExtendWith(MockitoExtension.class)
 class RateLimiterServiceTest {
 
-  @InjectMocks
   private RateLimiterService rateLimiterService;
 
   @Mock
@@ -25,6 +26,13 @@ class RateLimiterServiceTest {
 
   @Mock
   private RedisScript<Boolean> redisScript;
+
+  @BeforeEach
+  void setUp() {
+    MeterRegistry meterRegistry = new SimpleMeterRegistry();
+
+    rateLimiterService = new RateLimiterService(redisTemplate, redisScript, meterRegistry);
+  }
 
   @Test
   @DisplayName("Fail-Open: Redis 연결 실패 시 예외가 발생하지 않고 요청을 허용(true)해야 한다")
