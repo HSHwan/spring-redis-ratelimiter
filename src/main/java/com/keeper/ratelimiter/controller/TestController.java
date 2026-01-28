34ccc6bb37dccc5fae06a7fc5cdddc6c0bb106d2
HSHwan <tnghks2866@gmail.com>
2026-01-28 15:51:52 +0900

    feat: 테스트 API 엔드포인트 변경

--- src/main/java/com/keeper/ratelimiter/controller/TestController.java
+++ src/main/java/com/keeper/ratelimiter/controller/TestController.java
@@ -3,9 +3,11 @@ package com.keeper.ratelimiter.controller;
 import com.keeper.ratelimiter.annotation.RateLimit;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/test")
 public class TestController {
 
   /**
